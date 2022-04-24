b8d34fcc90395e3fd49d1ca3bd6f35f8eeaa526a
MagaliGiansiracusa <giansiracusamagali@gmail.com>
2022-04-24 17:34:22 -0300

    POST PUT y DELETE

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -2,24 +2,27 @@
 const fs = require("fs");
 const path = require("path");
 const productsFilePath = path.join(__dirname, "../data/products.json");
-const products = JSON.parse(fs.readFileSync(productsFilePath, "utf-8"));
+let products = JSON.parse(fs.readFileSync(productsFilePath, "utf-8"));
 
 const productsController = {
-
         products: (req, res) => {
-        res.render("index");
-        
+            res.render("index");
         },
-
         categories: (req, res) => {
             let categoryId = req.params.category;
             let productCategory = products.filter(producto => producto.category == categoryId);
-            
+            console.log(products);
             res.render('products/products', {productCategory, style: 'products.css'});
         },
-
         create: (req, res) =>{
             res.render('products/productCreate', {style: 'register.css'});
+        },
+        createPOST: (req, res) =>{
+            let newProduct = req.body;
+            newProduct.id = products.length + 1;
+            products.push(newProduct);
+            fs.writeFileSync(productsFilePath, JSON.stringify(products));
+            res.redirect('/products/' + newProduct.id);
         },
         productDetail: (req, res) =>{
             let productId = req.params.id;
@@ -30,11 +33,24 @@ const productsController = {
             res.render('products/productCart.ejs', {style: 'productCart.css'});
         },
         edit: (req, res) =>{
-            res.render('products/productEdit.ejs', {style: 'register.css'});
+            let productId = req.params.id;
+            let producto = products.find((producto) => producto.id == productId);
+            res.render('products/productEdit.ejs', {style: 'register.css', producto:producto});
+        },
+        editPUT: (req, res) =>{
+            let id = req.params.id;
+            let newProduct = req.body;
+            products = products.map(p => (p.id == id ? newProduct : p));
+            fs.writeFileSync(productsFilePath, JSON.stringify(products));
+            res.redirect('/products/' + id);  
+        },
+        delete: (req, res) =>{
+            let id = req.params.id;
+            products = products.filter(p => p.id != id);
+            fs.writeFileSync(productsFilePath, JSON.stringify(products));
+            res.redirect('/products');
         }
 }
-
-
 
 module.exports = productsController;
 
--- routes/products.js
+++ routes/products.js
@@ -5,8 +5,11 @@ const productsController = require('../controllers/productsController');
 router.get('/', productsController.products);     
 router.get('/category/:category', productsController.categories);  
 router.get('/create', productsController.create);
+router.post('/create', productsController.createPOST);
+router.get('/productCart', productsController.productCart); 
 router.get('/:id', productsController.productDetail);  
 router.get('/:id/edit', productsController.edit);
-router.get('/productCart', productsController.productCart); 
+router.put('/:id', productsController.editPUT);
+router.delete('/:id', productsController.delete)
 
 module.exports = router;
