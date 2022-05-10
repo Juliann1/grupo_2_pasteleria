24b50006227fa31a3b995c5e141d5f2fc7af6368
MagaliGiansiracusa <giansiracusamagali@gmail.com>
2022-05-09 23:16:07 -0300

    multer y vistas del detalle de usuario
    sobre el bcrypt, todavía no tira true el compareSync entre la contraseña y la confirmación de la contraseña

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const path = require("path");
 const bcrypt = require('bcryptjs');
+const res = require("express/lib/response");
 const usersFilePath = path.join(__dirname, "../data/users.json");
 let users = JSON.parse(fs.readFileSync(usersFilePath, "utf-8"));
 
@@ -12,22 +13,35 @@ const usersController ={
         res.render('users/register', {style: 'register.css'});
     },
     registerPOST: (req, res) =>{
-        let files = req.files.map((e) => `/img/${e.filename}`);
+        let files = req.files.map((e) => `/img/users/${e.filename}`);
 
         let encPassword = bcrypt.hashSync(req.body.password, 10);
-
+        let checkPassword = bcrypt.compareSync(req.body.password2, req.body.password);
+        console.log(req.body.password);
+        console.log(req.body.password2);
+        console.log(encPassword);
+        console.log(checkPassword);
         let newUser = {
             ...req.body,
             profile_pic: files[0]
         };
 
         newUser.password = encPassword;
+        newUser.password2 = checkPassword;
 
         newUser.id = String(users.length + 1);
         users.push(newUser);
 
         fs.writeFileSync(usersFilePath, JSON.stringify(users));
         res.redirect("/users/" + newUser.id);
+    },
+    userDetail: (req, res) => {
+        let userId = req.params.id;
+        let usuario = users.find((usuario) => usuario.id == userId);
+        res.render("users/userDetail", {
+            style: "userDetail.css",
+            usuario: usuario,
+        });
     }
 };
 
--- routes/users.js
+++ routes/users.js
@@ -7,12 +7,12 @@ const multer = require("multer");
 
 let storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        let folder = path.join(__dirname, "../public/img/user");
+        let folder = path.join(__dirname, "../public/img/users");
         cb(null, folder);
     },
     filename: (req, file, cb) => {
         // Tenemos que ver que nombre le vamos a poner a las fotos guardadas
-        const name = req.body.name.split(" ").join("-");
+        const name = req.body.nombreApellido.split(" ").join("-");
         const newFilename =
             name + "-" + Date.now() + path.extname(file.originalname);
         cb(null, newFilename);
@@ -23,6 +23,7 @@ let fileUpload = multer({ storage });
 
 router.get('/login', usersController.login);
 router.get('/register', usersController.register);
+router.get("/:id", usersController.userDetail);
 router.post('/register', fileUpload.any(), usersController.registerPOST);
 router.get("/:id", usersController.login);
 module.exports = router;
