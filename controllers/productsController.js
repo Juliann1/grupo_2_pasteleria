2b407b49e55f82115954fff35bd30e0c1d1314d1
Julian O <juliann_o@hotmail.com>
2022-06-17 17:55:38 -0300

    Cambio busqueda por ID, ahora lo hace con la DB

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -3,7 +3,7 @@ const path = require("path");
 const productsFilePath = path.join(__dirname, "../data/products.json");
 let products = JSON.parse(fs.readFileSync(productsFilePath, "utf-8"));
 const { validationResult } = require("express-validator");
-const db = require('../database/models')
+const db = require("../database/models");
 
 const productsController = {
     products: (req, res) => {
@@ -11,47 +11,49 @@ const productsController = {
     },
     categories: async (req, res) => {
         try {
-            let categoryName = req.params.category
+            let categoryName = req.params.category;
             let products = await db.Product.findAll({
-                include: [{
-                    association: 'category',
-                    where: {
-                        category: categoryName
-                    }
-                }],
-            })
+                include: [
+                    {
+                        association: "category",
+                        where: {
+                            category: categoryName,
+                        },
+                    },
+                ],
+            });
 
-            let productCategory = products.map(product => {
+            let productCategory = products.map((product) => {
                 return {
                     id: product.id,
                     name: product.product_name,
                     img1: JSON.parse(product.images)[0],
-                    precio: product.price
-                }
-            })
+                    precio: product.price,
+                };
+            });
 
             res.status(200).render("products/products", {
                 productCategory,
                 style: "products.css",
             });
         } catch (err) {
-            throw new Error(err)
-        } 
+            console.log(err);
+        }
     },
     create: (req, res) => {
         res.render("products/productCreate", { style: "register.css" });
     },
     createPOST: (req, res) => {
         let errors = validationResult(req);
-        
+
         if (!errors.isEmpty()) {
             return res.render("products/productCreate", {
                 style: "register.css",
                 errors: errors.mapped(),
-                oldData: req.body
+                oldData: req.body,
             });
         }
-       
+
         let files = req.files.map((e) => `/img/${e.filename}`);
 
         let newProduct = {
@@ -67,13 +69,31 @@ const productsController = {
         fs.writeFileSync(productsFilePath, JSON.stringify(products, null, " "));
         res.redirect("/products/" + newProduct.id);
     },
-    productDetail: (req, res) => {
-        let productId = req.params.id;
-        let producto = products.find((producto) => producto.id == productId);
-        res.render("products/productDetail", {
-            style: "productDetail.css",
-            producto: producto,
-        });
+    productDetail: async (req, res) => {
+        try {
+            let productId = req.params.id;
+            let product = await db.Product.findByPk(productId);
+            let delivery = await db.Shipping.findByPk(product.shipping_id);
+            let images = JSON.parse(product.images);
+
+            let producto = {
+                name: product.product_name,
+                description: product.product_description,
+                precio: product.price,
+                img1: images[0],
+                img2: images[1],
+                img3: images[2],
+                entrega: delivery.shipping,
+                consejos: product.tip,
+            };
+
+            res.status(200).render("products/productDetail", {
+                style: "productDetail.css",
+                producto,
+            });
+        } catch (err) {
+            console.log(err);
+        }
     },
     productCart: (req, res) => {
         res.render("products/productCart.ejs", { style: "productCart.css" });
